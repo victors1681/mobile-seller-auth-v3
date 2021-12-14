e4176382a48c2d916a03fbf3594092bd25645bf2
victors1681 <v.santos@itsoluclick.com>
2021-12-13 20:59:40 -0400

    lint pz

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -3,8 +3,8 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = () => (
   <div>
-      <NavLink to="/">Home </NavLink>
-    </div>
+    <NavLink to="/">Home </NavLink>
+  </div>
 );
 
 export default Navigation;
